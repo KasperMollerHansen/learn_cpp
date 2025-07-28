79c167764e2c6a2ace2631948dd5bfe75c5c8258
KasperMollerHansen <kasperhansen1998@hotmail.com>
2025-07-28 12:34:38 +0200

    test17

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <charconv> // C++17
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    const int year = 2025;
+    constexpr size_t buffer_size = 5; // 4 digits + null terminator (extra space is safe)
+    char buffer[buffer_size];
+    auto result = std::to_chars(buffer, buffer + buffer_size, year);
+    if (result.ec == std::errc()) {
+        std::cout << "Hello, World!\n";
+        std::cout << "This is the year " << std::string(buffer, result.ptr) << "\n";
+    } else {
+        std::cout << "Conversion failed\n";
+    }
 }
