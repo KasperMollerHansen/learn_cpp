e67556633d98aae8779d158062a61328c788f856
KasperMollerHansen <kasperhansen1998@hotmail.com>
2025-07-31 15:36:28 +0200

    update

---
+++ a3_integer_matrix/integer_matrix.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <utility>
+#include <stdexcept>
+#include <iostream>
+
+class Imatrix {
+    std::vector<std::vector<int>> data;
+    size_t rows, cols;
+
+public:
+    Imatrix(size_t r, size_t c);
+    Imatrix(const Imatrix& other);
+    Imatrix(Imatrix&& other) noexcept;
+    Imatrix& operator=(const Imatrix& other);
+    Imatrix& operator=(Imatrix&& other) noexcept;
+
+    int& operator()(size_t x, size_t y);
+
+    Imatrix operator+(const Imatrix& other) const;
+    Imatrix operator-(const Imatrix& other) const;
+    Imatrix operator*(const Imatrix& other) const;
+    Imatrix operator/(const Imatrix& other) const;
+    Imatrix operator%(const Imatrix& other) const;
+
+    void Move(std::pair<size_t, size_t> src, std::pair<size_t, size_t> dst);
+    std::vector<int> Row(size_t n) const;
+    std::vector<int> Column(size_t n) const;
+    void Print() const;
+    void FillRandom(int max_value);
+};
