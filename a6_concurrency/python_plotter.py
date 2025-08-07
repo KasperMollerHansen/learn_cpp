7ba6b0f62442eb2b851bbc66eaa1bf4df9cf5374
KasperMollerHansen <kasperhansen1998@hotmail.com>
2025-08-07 09:39:40 +0200

    finish

--- a6_concurrency/python_plotter.py
+++ a6_concurrency/python_plotter.py
@@ -53,3 +53,27 @@ fig3.update_layout(
 )
 fig3.write_image(os.path.join(plot_dir, "thread_scaling_large.png"))
 fig3.show()
+#%%
+def save_table(df, title, filename):
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=[f"<b>{col}</b>" for col in df.columns],
+                    fill_color='paleturquoise',
+                    align='center'),
+        cells=dict(values=[df[col] for col in df.columns],
+                   fill_color='lavender',
+                   align='center'))
+    ])
+    fig.update_layout(title=title)
+    fig.write_image(os.path.join(plot_dir, filename))
+
+# --- Serial vs Parallel Table ---
+df = pd.read_csv(os.path.join(script_dir, "output_data/results_serial_vs_parallel.csv"), comment='/')
+save_table(df, "Serial vs Parallel vs Parallel Ready (Raw Data)", "serial_vs_parallel_table.png")
+
+# --- Thread Scaling (Small) Table ---
+df_small = pd.read_csv(os.path.join(script_dir, "output_data/results_thread_scaling_small.csv"), comment='/')
+save_table(df_small, "Thread Scaling (Small N) (Raw Data)", "thread_scaling_small_table.png")
+
+# --- Thread Scaling (Large) Table ---
+df_large = pd.read_csv(os.path.join(script_dir, "output_data/results_thread_scaling_large.csv"), comment='/')
+save_table(df_large, "Thread Scaling (Large N) (Raw Data)", "thread_scaling_large_table.png")
